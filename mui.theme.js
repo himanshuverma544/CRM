cd548a5989e2dede4489019961a3fb84b9069be3
Himanshu Verma <himanshuverma544@gmail.com>
2024-04-16 19:56:27 +0530

    upd: added colors for the app

--- mui.theme.js
+++ mui.theme.js
@@ -1,10 +1,29 @@
 import { createTheme } from "@mui/material";
 
 const theme = createTheme({
-  breakpoints: {
 
-    /* Subtracting 1px, as MUI 639px is equal to Tailwind 640px */
+  palette: {
+    primary: {
+      main: '#092661',
+      body: '#F9FBFE',
+      link: '#FFF',
+      text: '#000'
+    },
+    secondary: {
+      main: '#1F3663',
+      link: '#b7c3df',
+      text: '#324253'
+    },
+    tertiary: {
+      text: '#8F9FBC'
+    },
+    green: '#22C55E',
+    yellow: '#EAB308',
+    blue: '#3B82F6',
+    red: '#EF4444'
+  },
 
+  breakpoints: {  // Subtracting 1px, as MUI 639px is equal to Tailwind 640px
     values: {
       sm: 640 - 1, 
       md: 768 - 1,
@@ -15,6 +34,4 @@ const theme = createTheme({
   },
 });
 
-export { 
-  theme
-};
+export default theme;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,20 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mainPrimary: "#092661",
+        bodyPrimary: "#F9FBFE",
+        linkPrimary: "#FFF",
+        textPrimary: "#000",
+  
+        mainSecondary: "#1F3663",
+        linkSecondary: "#b7c3df",
+        textSecondary: "#324253",
+  
+        textTertiary: "#8F9FBC"
+      }
+    }
   },
   plugins: [],
 }
