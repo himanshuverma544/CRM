380766b4fb0204f2e083c63da1ff7da4764cfa88
Himanshu Verma <himanshuverma544@gmail.com>
2024-04-18 13:18:22 +0530

    add: created file for external links

---
+++ src/externalLinks.js
@@ -0,0 +1,14 @@
+const ABOUT_DEVELOPER = {
+  path: "https://www.linktr.ee/himanshuverma544",
+  text: "About Developer"
+}
+
+const SOURCE_CODE = {
+  path: "https://www.github.com/himanshuverma544/CRM",
+  text: "Source Code"
+}
+
+export {
+  ABOUT_DEVELOPER,
+  SOURCE_CODE
+};
