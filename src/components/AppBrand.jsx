292da2a3a994e01d7cbe69552c67a8b0cbd303f1
Himanshu Verma <himanshuverma544@gmail.com>
2024-04-18 13:19:57 +0530

    rfc: rectified the component and used the LinkItem component

---
+++ src/components/AppBrand.jsx
@@ -0,0 +1,28 @@
+import { DASHBOARD } from "../routes";
+
+import { useTheme } from '@mui/material/styles';
+
+import LinkItem from "./LinkItem";
+
+
+const AppBrand = ({ className: linkClassName, name, logo, variant }) => {
+
+  const theme = useTheme();
+
+  return (
+    <LinkItem
+      className={{
+        link: linkClassName,
+        linkText: "name tracking-widest"
+      }}
+      path={DASHBOARD.path}
+      linkText={name}
+      icon={logo}
+      variant={variant}
+      fontSize="2rem"
+      color={theme.palette.primary.link}
+    />
+  );
+}
+
+export default AppBrand;
