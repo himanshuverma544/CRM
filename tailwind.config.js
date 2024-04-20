5e777d3925a4a7f1dbeffd38be5d53d600ecc17b
Himanshu Verma <himanshuverma544@gmail.com>
2024-04-20 12:12:44 +0530

    upd: set the screen breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,14 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
+    screens: {
+      'xs': '375px',
+      'sm': '425px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1440px',
+      '2xl': '2560px'
+    },
     extend: {
       colors: {
         mainPrimary: "#092661",
