d0a720364b11e10f6cbf12e7da9f1071e88da50d
Himanshu Verma <himanshuverma544@gmail.com>
2024-04-20 12:17:08 +0530

    upd: - changed to stack component - made responsive - added typography component

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,10 +1,17 @@
-import { NavLink, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 import { DASHBOARD, PRODUCTS, CUSTOMERS, SIGN_IN } from '../routes';
 
+import { ABOUT_DEVELOPER, SOURCE_CODE } from "../externalLinks";
+
+import { useTheme } from '@mui/material/styles';
+
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
+import Stack from "@mui/material/Stack";
 import Box from "@mui/material/Box";
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
 
 import WebhookIcon from '@mui/icons-material/Webhook';
 import HomeIcon from '@mui/icons-material/Home';
@@ -15,25 +22,38 @@ import GroupsIcon from '@mui/icons-material/Groups';
 import GroupsOutlinedIcon from '@mui/icons-material/GroupsOutlined';
 import LoginIcon from '@mui/icons-material/Login';
 import LoginOutlinedIcon from '@mui/icons-material/LoginOutlined';
+import Person4OutlinedIcon from '@mui/icons-material/Person4Outlined';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 import AppBrand from "../components/AppBrand";
 import NavItem from "../components/NavItem";
+import LinkItem from '../components/LinkItem';
 
 
 const Layout = () => {
+
+  const theme = useTheme();
 
   return (
     <>
       <AppBar className="header" position='static'>
         <Toolbar>
-          <Box className="w-full flex justify-around" component="nav">
+          <Stack 
+            className="w-full" 
+            component="nav"
+            justifyContent="space-around"
+          >
             <AppBrand
-              className="app-brand nav-links w-3/12 flex justify-center items-center gap-2"
+              className="app-brand w-6/12 lg:w-3/12 flex justify-center items-center gap-2"
               logo=<WebhookIcon className="logo" fontSize="inherit"/> 
               name="CRM"
-              variant="h1"
             />
-            <Box className="nav-links w-6/12 flex justify-around" component="ul">
+            <Stack 
+              className="nav-links w-full lg:w-6/12 fixed lg:static bottom-0 bg-mainPrimary" 
+              component="ul"
+              justifyContent="space-around"
+              alignItems="center"
+            >
               <NavItem
                 path={DASHBOARD.path}
                 activeIcon={<HomeIcon/>}
@@ -52,16 +72,22 @@ const Layout = () => {
                 inactiveIcon={<GroupsOutlinedIcon/>}
                 title={CUSTOMERS.title}
               />
-            </Box>
-            <Box className="nav-links w-3/12 flex justify-evenly" component="ul">
+            </Stack>
+            <Stack 
+              className="nav-links w-6/12 lg:w-3/12"
+              component="ul"
+              justifyContent="space-evenly"
+              alignItems="center"
+            >
               <NavItem
                 path={SIGN_IN.path}
                 activeIcon={<LoginIcon/>}
                 inactiveIcon={<LoginOutlinedIcon/>}
                 title={SIGN_IN.title}
+                keepInRow={true}
               />
-            </Box>
-          </Box>
+            </Stack>
+          </Stack>
         </Toolbar>
       </AppBar>
 
@@ -70,10 +96,42 @@ const Layout = () => {
         <Outlet/>
       </Box>
 
-      <Box component="footer">
-         
-      </Box>
+      <Paper 
+        className="flex flex-wrap justify-around items-center px-6 py-2 mb-14 lg:mb-0" 
+        component="footer" 
+        square={true} 
+        elevation={24} 
+        sx={{backgroundColor: theme.palette.primary.main}}
+      >
+        <LinkItem 
+          className={{link: "flex items-center gap-2"}}
+          path={ABOUT_DEVELOPER.path}
+          linkText={ABOUT_DEVELOPER.text}
+          icon={<Person4OutlinedIcon/>}
+          target="_blank"
+          hoverEffect={true}
+        />          
 
+        <Typography 
+          className="p-3 w-full sm:w-fit text-center" 
+          component="span" 
+          color={theme.palette.quaternary.text}
+          sx={{'@media (max-width: 545px)': {
+            order: 3
+          }}}
+        >
+          Made with Love ❤
+        </Typography>
+
+        <LinkItem
+          className={{link: "flex justify-end items-center gap-2"}}
+          path={SOURCE_CODE.path}
+          linkText={SOURCE_CODE.text}
+          icon={<GitHubIcon/>}
+          target="_blank"
+          hoverEffect={true}
+        />
+      </Paper>
     </>
   );
 }
