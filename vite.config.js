e951041ca985dd037cb4ef6ad0cea337247e6929
Himanshu Verma <himanshuverma544@gmail.com>
2024-04-22 23:00:23 +0530

    add: added the svg support

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+import svgr from 'vite-plugin-svgr';
 import eslint from 'vite-plugin-eslint';
 
 
@@ -8,6 +9,12 @@ export default defineConfig({
 
   plugins: [
     react(),
+
+    svgr({
+      svgrOptions: {
+        ref: true
+      }
+    }),
 
     eslint({
       cache: false,
